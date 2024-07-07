5c850872c60cec763090202bc9dd3b0378363964
VinayPurohit31 <vanaypurohit31@gmail.com>
2024-07-07 22:12:37 +0530

    madlib working

---
+++ Madlib1/college.py
@@ -0,0 +1,8 @@
+ajective=input("Enter a Ajective = ")
+verb1=input("Enter a  Verb = ")
+verb2=input("Enter a second Verb = ")
+enter_name_famous=input("Enter a famous person name = ")
+
+madlib=f"want to college is so {ajective}. i want {verb1} my self to the {verb2} like {enter_name_famous}"
+
+print(madlib)
---
+++ random_madlib.py
@@ -0,0 +1,5 @@
+
+import random
+from Madlib1 import creation,adventure, college
+m = random.choice([college,adventure,creation])
+m.madlib()
